de9da9fa4afb9bb683d9316a6ec706606723fd8c
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-04-01 12:32:18 +0700

    menambahkan looping

--- file.cpp
+++ file.cpp
@@ -7,4 +7,11 @@ int menuPilihan;
 bool prima(int x){
     if (x <= 1) return false;
     int i = 2;
+    while (i <= x /2){
+        if (x % i == 0){            //Kalau n bisa dibagi i (habis dibagi) : berarti bukan prima
+            return false;           //hasilnya bukan prima
+        }
+        i++;
+    }
+    return true;
 }
